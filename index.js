6ca5c61b4858ee341cecb360754976c1fe419e16
Unknown <mj0857363@otc.edu>
2017-09-20 12:45:48 -0500

    Question fix

--- index.js
+++ index.js
@@ -17,20 +17,23 @@ let questions = [{
     message: "What toppings?",
     default: "Plain Cheese",
     choices: [" Plain Cheese", " Pepperoni", " Sausage", " Bell Peppers", " Olives", " Mushrooms"]},
-    {type: "list",
-    name: "check",
-    message: "Is your order correct?",
-    choices: ["yes", "no"]}
+    // {type: "list",
+    // name: "check",
+    // message: "Is your order correct?",
+    // choices: ["yes", "no"]}
     ]
 
 console.log(chalk.blue("Welcome to Hungry Hippos' Pizza!"))
 
 inquirer.prompt(questions).then(function(answers){
-    console.log(chalk.magenta("Your order is a " + answers.size + " pizza with " + answers.sauce + " and " + answers.toppings))
-    if(answers.check == "yes"){
-        console.log(chalk.magenta("It's on its way!!"))
-    }
-    if(answers.check == "no"){
-        console.log(chalk.red("We apologize for the inconvenience."))
-    }
+    console.log(chalk.magenta("Your order is a " + answers.size + " pizza with " + answers.sauce + " and" + answers.toppings))
+    
+    inquirer.prompt({type: "list", name: "check", message: "Is your order correct?", choices: ["yes", "no"]}).then(function(answers){
+        if(answers.check == "yes"){
+            console.log(chalk.magenta("It's on its way!!"))
+        }
+        if(answers.check == "no"){
+            console.log(chalk.red("We apologize for the inconvenience."))
+        }
+    })
 })
