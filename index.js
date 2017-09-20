0bc92dab77210575cdb7c9ea801976bec050281e
Unknown <mj0857363@otc.edu>
2017-09-20 12:33:25 -0500

    All the rest!

--- index.js
+++ index.js
@@ -1,15 +1,36 @@
 var inquirer = require('inquirer')
 var chalk = require('chalk')
 
-let questions = size = { 
+let questions = [{ 
     type: "list",
     name: "size",
-    message: "What size of pizza",
+    message: "What size of pizza?",
     default: "Small",
-    choices: ["Small", "Medium", "Large"]}
+    choices: ["Small", "Medium", "Large"]},
+    {type: "list",
+    name: "sauce",
+    message: "What type of sauce?",
+    defualt: "Marinara",
+    choices: ["Marinara", "BBQ", "Alfredo"]},
+    {type: "checkbox",
+    name: "toppings",
+    message: "What toppings?",
+    default: "Plain Cheese",
+    choices: [" Plain Cheese", " Pepperoni", " Sausage", " Bell Peppers", " Olives", " Mushrooms"]},
+    {type: "list",
+    name: "check",
+    message: "Is your order correct?",
+    choices: ["yes", "no"]}
+    ]
+
+console.log(chalk.blue("Welcome to Hungry Hippos' Pizza!"))
 
 inquirer.prompt(questions).then(function(answers){
-    
+    console.log(chalk.green("Your order is a " + answers.size + " pizza with " + answers.sauce + " and " + answers.toppings))
+    if(answers.check == "yes"){
+        console.log(chalk.green("It's on its way!!"))
+    }
+    if(answers.check == "no"){
+        console.log(chalk.red("We apologize for the inconvenience."))
+    }
 })
-
-Console.log(size);
